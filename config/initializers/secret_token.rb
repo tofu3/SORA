494dd70de7db027aa3467a8679040d9ae2cfb4c7
Nils Masen <nils@masen.name>
2010-09-03 14:31:56 +0200

    Basic RAILS NEW commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sora::Application.config.secret_token = 'b22844907b49dc29f12e8ddfab794061b22a3bbe6cf4ca2862ea188c2f8bc9126afbde6099169c24bed68aaca70b9d434e4c95659c15face0753d1624092d53f'
